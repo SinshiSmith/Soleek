1453e4eb504f94d1a97815feea7fe1c2aa6b8b6a
Sinshi Smith <yorfnd@gmail.com>
2018-10-19 18:36:17 +0200

    added path for new product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { NewProductComponent } from './new-product/new-product.component';
 
 const routes: Routes = [
-  {path: '', component: ProductsComponent}
+  {path: '', component: ProductsComponent},
+  {path: 'new', component: NewProductComponent},
 ];
 
 @NgModule({
