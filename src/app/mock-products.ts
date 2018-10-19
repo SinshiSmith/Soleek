db64453c330a87298bbe5cb14dddab9bc0ed579b
Sinshi Smith <yorfnd@gmail.com>
2018-10-19 18:06:12 +0200

    created a fake data file

---
+++ src/app/mock-products.ts
@@ -0,0 +1,44 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+  {
+    sku: 1,
+    name: "Apple",
+    image: "img",
+    categories: ["Fruit"],
+    price: 10,
+    date: "21-10-2018"
+  },
+  {
+    sku: 2,
+    name: "Orange",
+    image: "img",
+    categories: ["Fruit"],
+    price: 10,
+    date: "21-10-2018"
+  },
+  {
+    sku: 3,
+    name: "Avocado",
+    image: "img",
+    categories: ["Fruit"],
+    price: 10,
+    date: "21-10-2018"
+  },
+  {
+    sku: 4,
+    name: "Banana",
+    image: "img",
+    categories: ["Fruit"],
+    price: 10,
+    date: "21-10-2018"
+  },
+  {
+    sku: 5,
+    name: "Strawberries",
+    image: "img",
+    categories: ["Fruit"],
+    price: 10,
+    date: "21-10-2018"
+  },
+];
