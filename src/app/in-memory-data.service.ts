646c5196acb52cd3abe53e554685f24ce7675b8b
Sinshi Smith <yorfnd@gmail.com>
2018-10-20 15:52:25 +0200

    added images

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,7 +7,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id:1,
         name: "Apple",
-        image: "img",
+        image: "https://images.unsplash.com/photo-1512578659172-63a4634c05ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8b54ab3603a1ae291af81311a486244d&auto=format&fit=crop&w=1350&q=80",
         categories: ["Fruit"],
         price: 10,
         date: "2018-10-19"
@@ -15,7 +15,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 2,
         name: "Orange",
-        image: "img",
+        image: "https://images.unsplash.com/photo-1512578000375-660f001ef96d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b2dc6766b432cab064622012b1b13a08&auto=format&fit=crop&w=1350&q=80",
         categories: ["Fruit"],
         price: 10,
         date: "2018-10-19"
@@ -23,7 +23,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 3,
         name: "Avocado",
-        image: "img",
+        image: "https://images.unsplash.com/photo-1519162808019-7de1683fa2ad?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a88f91a723a209683b18615b9f30e589&auto=format&fit=crop&w=1268&q=80",
         categories: ["Fruit"],
         price: 10,
         date: "2018-10-19"
@@ -31,7 +31,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 4,
         name: "Banana",
-        image: "img",
+        image: "https://images.unsplash.com/photo-1536228891610-d27ef66f7110?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d96577550553d67c983162f7d3aa7b15&auto=format&fit=crop&w=1350&q=80",
         categories: ["Fruit"],
         price: 10,
         date: "2018-10-19"
@@ -39,7 +39,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 5,
         name: "Strawberries",
-        image: "img",
+        image: "https://images.unsplash.com/photo-1525604155062-d5b01d84ad81?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c3b5d6a78ca4204519dcf15c11f40d90&auto=format&fit=crop&w=1267&q=80",
         categories: ["Fruit"],
         price: 10,
         date: "2018-10-19"
