b0791591319f74e9dd728db2a341378d188b2577
Sinshi Smith <yorfnd@gmail.com>
2018-10-20 13:48:02 +0200

    built a form and the ngmodel for a basic functionality

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -1,4 +1,9 @@
+import { ProductsService } from './../products.service';
+import { Product } from './../product';
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-new-product',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.component.css']
 })
 export class NewProductComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private location: Location
+    ) {}
+  onSubmit(product) {
+    if (!product) { return; }
+    product= this.clear(product);
+    this.productsService.addProduct(product as Product).subscribe(() => this.location.back());
+    // console.log(product); 
+  }
+  
+  clear(product) {
+    product.name = product.name.trim();
+    product.categories = product.categories.trim();
+    product.categories = product.categories.split(" ");
+    product.image = product.image.trim();
+    return product;
+  }
+  
   ngOnInit() {
+    // this.productsService.getProduct(1).subscribe(product => this.model = product);
   }
 
 }
