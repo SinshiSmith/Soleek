7a611a283abfc74378e4097f3433661acb2e286a
Sinshi Smith <yorfnd@gmail.com>
2018-10-20 15:07:54 +0200

    added edit path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EdtProductComponent } from './edt-product/edt-product.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
@@ -6,6 +7,7 @@ import { NewProductComponent } from './new-product/new-product.component';
 const routes: Routes = [
   {path: '', component: ProductsComponent},
   {path: 'new', component: NewProductComponent},
+  {path: 'edit/:id', component: EdtProductComponent},
 ];
 
 @NgModule({
